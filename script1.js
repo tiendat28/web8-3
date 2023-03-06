3485721d19476a992c6116338d7d30a3915a7356
tiendat28 <datc3.cc@gmail.com>
2023-03-06 23:22:23 +0700

    chau

--- script1.js
+++ script1.js
@@ -4,7 +4,7 @@ function checkLogin() {
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
 
-	if (username === "tien dat" && password === "haha") {
+	if (username === "châu không gầy " && password === "chauxinhkhongbeo") {
 		window.location.href = "index.html";
 	} else {
 		alert("Sai tên đăng nhập hoặc mật khẩu!");
