fd9e16838363a6779b9cb0154a3bb7f04364e31e
tiendat28 <datc3.cc@gmail.com>
2023-03-08 01:47:09 +0700

    lab

--- script1.js
+++ script1.js
@@ -7,8 +7,32 @@ function checkLogin() {
 	if (username === "châu không gầy" && password === "chauxinhkhongbeo") {
 		window.location.href = "index.html?name=chau";
 	}
-	else if (username === "dat" && password === "haha") {
-		window.location.href = "index.html?name=dat";
+	else if (username === "boss xinh đẹp" && password === "dungnhansaicai") {
+		window.location.href = "index.html?name=sepquynh";
+	}
+	else if (username === "thư bìm bịp" && password === "dungnhansaicai") {
+		window.location.href = "index.html?name=thubip";
+	}
+	else if (username === "trang ngố" && password === "dungnhansaicai") {
+		window.location.href = "index.html?name=trangluytinh";
+	}
+	else if (username === "huyền xinh gái" && password === "loveyou") {
+		window.location.href = "index.html?name=huyenxinhgai";
+	}
+	else if (username === "hiền ngáo đá" && password === "dungnhansaicai") {
+		window.location.href = "index.html?name=hien";
+	}
+	else if (username === "chị trang xinh gái" && password === "dungnhansaicai") {
+		window.location.href = "index.html?name=chitrang";
+	}
+	else if (username === "bich xinh gái" && password === "dungnhansaicai") {
+		window.location.href = "index.html?name=bich";
+	}
+	else if (username === "hạnh đáng yêu" && password === "dungnhansaicai") {
+		window.location.href = "index.html?name=hanh";
+	}
+	else if (username === "minh anh xinh gái" && password === "dungnhansaicai") {
+		window.location.href = "index.html?name=minhanh";
 	}
 	else {
 		alert("Sai tên đăng nhập hoặc mật khẩu!");
