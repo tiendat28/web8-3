92c2aca165e4524458978001112446c6af12efc8
bangtx <bangtx@otani-trading.com>
2023-03-07 20:53:59 +0700

    fix images

--- script1.js
+++ script1.js
@@ -4,9 +4,13 @@ function checkLogin() {
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
 
-	if (username === "châu không gầy " && password === "chauxinhkhongbeo") {
-		window.location.href = "index.html";
-	} else {
+	if (username === "châu không gầy" && password === "chauxinhkhongbeo") {
+		window.location.href = "index.html?name=chau";
+	}
+	else if (username === "dat" && password === "haha") {
+		window.location.href = "index.html?name=dat";
+	}
+	else {
 		alert("Sai tên đăng nhập hoặc mật khẩu!");
 	}
 }
